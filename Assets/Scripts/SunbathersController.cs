66945005dcefb7c8d8669fd429189e54e44b2159
vevuo <vesa.vuoristo@gmail.com>
2018-08-03 11:29:11 +0300

    Adding some sunbathers and drink bubble

---
+++ Assets/Scripts/SunbathersController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SunbathersController : MonoBehaviour {
+    private float sunburnTime = 0f;
+    private float hydration = 100.0f;
+    public GameObject sunbather;
+    public GameObject drinkbubble;
+
+    // Use this for initialization
+    void Start()
+    {
+        sunburnTime = GetRandom();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.isActiveAndEnabled) {
+            if (sunburnTime > 0.0f)
+            {
+                sunburnTime -= Time.deltaTime * 3.0f;
+            }
+            else
+            {
+                sunbather.SetActive(false);
+            }
+
+            if (hydration > 0.0f)
+            {
+                hydration -= Time.deltaTime;
+                if (hydration < 50.0f) {
+                    orderDrink();
+                }
+            }
+            else {
+                sunbather.SetActive(false);
+            }
+        }
+    }
+
+    private float GetRandom()
+    {
+        return Random.Range(20.0f, 40.0f);
+    }
+
+    private void orderDrink()
+    {
+        drinkbubble.SetActive(true);
+    }
+}
