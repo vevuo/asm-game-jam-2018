4afb452d9cfcb5d093941c8f642e0ff265bf2cb9
Juhani Halkomäki <juhani.halkomaki@gmail.com>
2018-08-03 19:47:02 +0300

    Add player animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,8 +1,11 @@
-﻿﻿using System.Collections;
+﻿﻿﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+    private Animator animator;
+    private bool direction = false;
+
     private int money = 0;
     private Rigidbody2D rb2d;
     public float maxSpeed = 30.0f;
@@ -11,12 +14,13 @@ public class PlayerController : MonoBehaviour {
     // Use this for initialization
     void Start () {
         rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void FixedUpdate() {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-        if (moveHorizontal != 0.0f || moveVertical != 0.0f) {
+        if (Mathf.Abs(moveHorizontal) > 0 || Mathf.Abs(moveVertical) > 0) {
             Vector2 movement = new Vector2(moveHorizontal, moveVertical);
 
             if (movement.magnitude > 0)
@@ -25,10 +29,21 @@ public class PlayerController : MonoBehaviour {
                     rb2d.AddRelativeForce(movement * accel);
             }
         }
+
+        animator.SetFloat("speed", rb2d.velocity.magnitude);
+        animator.SetBool("direction", rb2d.velocity.x > 0);
+
+        if((direction && rb2d.velocity.x > 0) ||
+           (!direction && rb2d.velocity.x < 0)){
+            Vector3 v = transform.localScale;
+            v.x *= -1;
+            transform.localScale = v;
+            direction = !direction;
+        }
     }
 
     void Update () {
-
+        
     }
 
     void OnCollisionEnter2D(Collision2D coll)
