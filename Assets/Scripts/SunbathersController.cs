977306bfe600be49d385a84734982190c82d7f66
Juhani Halkomäki <juhani.halkomaki@gmail.com>
2018-08-03 15:53:54 +0300

    Make the sunbathers red

--- Assets/Scripts/SunbathersController.cs
+++ Assets/Scripts/SunbathersController.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class SunbathersController : MonoBehaviour {
+    private const float maxSunBurn = 10f;
+    private const float sunLotionRelief = -1f;
+
+    private bool isInShade = false;
+    private float amountOfSunLotion = 0f;
+    private float sunburn = 0f;
+    private float sunburnRate = 1f;
+
     private float sunburnTime = 0f;
     private float hydration = 0f;
     public GameObject sunbather;
     public GameObject drinkbubble;
 
-    // Use this for initialization
-    void Start()
+    private SpriteRenderer sr;
+    private Color redSkin = new Color(1,0,0);
+
+	private void Awake()
+	{
+        sr = GetComponent<SpriteRenderer>();
+	}
+
+	// Use this for initialization
+	void Start()
     {
         sunburnTime = Random.Range(70.0f, 120.0f);
         hydration = Random.Range(40.0f, 60.0f);
@@ -19,6 +35,25 @@ public class SunbathersController : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+        if(isInShade){
+            
+        }
+        else if (amountOfSunLotion > 0)
+        {
+            sunburn += sunLotionRelief * Time.deltaTime;
+            amountOfSunLotion -= Time.deltaTime;
+        }
+        else
+        {
+            sunburn += sunburnRate * Time.deltaTime;
+            if (sunburn < 0)
+            {
+                sunburn = 0;
+            }
+        }
+
+        sr.color = Color.Lerp(Color.white, redSkin, sunburn / maxSunBurn);
+
         if (this.isActiveAndEnabled) {
             if (sunburnTime > 0.0f)
             {
@@ -40,6 +75,8 @@ public class SunbathersController : MonoBehaviour {
                 sunbather.SetActive(false);
             }
         }
+
+
     }
 
     private void orderDrink()
