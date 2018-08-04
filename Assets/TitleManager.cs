5effa2911a943f980491ee4467cb5a1d9cff06ac
Juhani Halkomäki <juhani.halkomaki@gmail.com>
2018-08-04 18:28:46 +0300

    Add title screen

---
+++ Assets/TitleManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleManager : MonoBehaviour {
+    public GameObject start;
+    private float counter;
+
+
+	// Use this for initialization
+	void Start () {
+        counter = 0;
+        start.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (counter < 3f)
+        {
+            counter += Time.deltaTime;
+        }
+        else
+        {
+            start.SetActive(true);
+            if (Input.GetButtonUp("Fire1"))
+            {
+                Debug.Log("röh");
+                SceneManager.LoadScene("SunbathersDrink");
+                //enabled = false;
+            }
+        }
+	}
+}
